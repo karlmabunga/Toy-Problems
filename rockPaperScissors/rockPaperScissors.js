cd9aabb97c819d756aeee2d257b59bd5d883ca49
Karl Mabunga <karlmabunga@yahoo.com>
2020-08-25 20:00:00 -0700

    Attempted the toy problem

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,34 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+// inputs - number of rounds
+// outputs - array of string output combinations
+// constraints - none
+// edge cases -none
+var rockPaperScissors = function (rounds) {
+  // create result var array
+  var result = [];
+  // create list of options players can choose from
+  var options = ['R', 'P', 'S'];
+  // create empty string to add options to push into result
+  var combinations = '';
+  // iterate through the options
+  for (var i = 0; i < options.length; i++) {
+  // while string is not more than length of rounds
+    while (combinations.length < rounds) {
+    // empty string is added each element
+    combinations += options[i];
+    }
+    // string is pushed into result array
+    if (result.indexOf(combinations) === -1) {
+    result.push(combinations);
+    }
+  }
+
+  // return result
+  return result;
 };
 
+
+
+// rockPaperScissors(3);
