78a138ac9be75e271a6a7cf0de86abf698720a37
Karl Mabunga <karlmabunga@yahoo.com>
2020-10-30 20:08:48 -0700

    Attempt

--- islandCount/islandCount.js
+++ islandCount/islandCount.js
@@ -1,5 +1,5 @@
 /**
- * Given a string representation of a 2d map, return the number of islands in the map. 
+ * Given a string representation of a 2d map, return the number of islands in the map.
  * Land spaces are denoted by a zero, while water is denoted by a dot. Two land spaces
  * are considered connected if they are adjacent (but not diagonal).
  *
@@ -7,4 +7,13 @@
 
 function countIslands(mapStr) {
   // Your code here.
+  var count = 0;
+  for (var i = 0; i < mapStr.length; i++) {
+    if (mapStr[i] = '0' && mapStr[i - 1] = '.' && mapStr[i - 1] = '.') {
+      count++;
+    }
+  }
+  return count;
 }
+
+
--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -1,10 +1,10 @@
 /**
- *  
- *  A robot located at the top left corner of a 5x5 grid is trying to reach the 
- *  bottom right corner. The robot can move either up, down, left, or right, 
- *  but cannot visit the same spot twice. How many possible unique paths are 
- *  there to the bottom right corner? 
- *  
+ *
+ *  A robot located at the top left corner of a 5x5 grid is trying to reach the
+ *  bottom right corner. The robot can move either up, down, left, or right,
+ *  but cannot visit the same spot twice. How many possible unique paths are
+ *  there to the bottom right corner?
+ *
  *  make your solution work for a grid of any size.
  *
  */
@@ -29,5 +29,24 @@ var makeBoard = function(n) {
 };
 
 var robotPaths = function(n, board, i, j) {
+  board = board || makeBoard(n)
+  i = i || 0;
+  j = j || 0;
+  // base case out of bounds or visited
+  if (!(i >= 0 && i < n && j >= 0 && j < n) || board.hasBeenVisited(i,j)) {
+    return 0;
+  }
+  // base case path found
+  if (i === n && j === n -1) {
+    return 1
+  }
+  // recursive case: toggle the piece and continue
+  board.togglePiece(i, j);
+  var result = robotPaths(n, board, i, j+1) + robotPaths(n, board, i, j-1) + robotPaths(n, board, i+1, j) + robotPaths(n, board, i-1, j);
+
+  // return the board to its original state
+  board.togglePiece(i,j);
+  return result;
+
 };
 
