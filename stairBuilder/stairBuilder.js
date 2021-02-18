b0406f485c6e9c165bde614c4626ef7b4cd8acb8
Karl Mabunga <karlmabunga@yahoo.com>
2021-02-18 12:52:49 -0800

    Added stair builder problem

---
+++ stairBuilder/stairBuilder.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should create an array of step shapes
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {}
+
